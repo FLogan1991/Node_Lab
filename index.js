f81c0a08794a92c18fb642530332a1cf15949aef
Logan Furlong <furlong_logan@columbusstate.edu>
2023-09-25 03:20:27 +0000

    Added param and update home.ejs

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ app.get('/', function( req, res ){
     res.render('home.ejs')
 })
 
+app.get('/path/name', function( req, res ){
+    //change to another ejs file
+    let name = req.params.name
+    console.log(name)
+    res.render('home.ejs')
+})
+
+app.get('/query', function( req, res ){
+    //change to another ejs file
+    let name = req.query.name
+    console.log(name)
+    res.render('home.ejs')
+})
+
 app.listen(3000, () => {
     console.log('started on port 3000')
 })
+
+
+// path param REQUIRED /home/678954390/...
+// query param /home?foo=bar&bar=foo
+//PUT & POST/CREATE & UPDATE body, from a form
