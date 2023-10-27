e439f2f3ba89047ad87fe6ce5ab7c46fc12438b4
Logan Furlong <furlong_logan@columbusstate.edu>
2023-10-27 04:55:45 +0000

    Set up a pass into the template

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.set("view engine", "ejs");
 app.get('/', function( req, res ){
     axios.get('https://xkcd.com/info.0.json').then(function(response){
         console.log(response.data);
+        res.render('home.ejs', {"name": null, xkcdDate: Response.data});
     })
-    res.render('home.ejs', {"name": null});
 })
 //added null in get above
 app.get('/path/:name', function( req, res ){
