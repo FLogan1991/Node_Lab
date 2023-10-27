3221764ed5f86e6c7d97b925d80ff26540a15b89
Logan Furlong <furlong_logan@columbusstate.edu>
2023-10-27 05:03:48 +0000

    fix to response.data

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 app.get('/', function( req, res ){
     axios.get('https://xkcd.com/info.0.json').then(function(response){
         console.log(response.data);
-        res.render('home.ejs', {name: null, xkcdData: Response.data});
+        res.render('home.ejs', {name: null, xkcdData: response.data});
     })
 })
 //added null in get above
